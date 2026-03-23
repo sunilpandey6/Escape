15713155d822dd5cd5aa11084af90058ae84f20e
Sunil <su2207pa-s@student.lu.se>
2026-03-23 16:16:49 +0100

    small update
    function UI working ongoing

---
+++ Assets/Script/Keyval.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keyval : MonoBehaviour
+{
+    public string value;
+    public bool isDelete;
+    public bool isNext;
+
+
+
+
+}
--- Assets/Script/UI_fnc.cs
+++ Assets/Script/UI_fnc.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UI_fnc : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{    
+    public void ShowPanel()
     {
-        
+        //Transform cam = ovrRig.centerEyeAnchor;
+        Vector3 spawnPos = cam.position + cam.forward * panelDistance;
+        Quaternion spawnRot = Quaternion.LookRotation(cam.forward, cam.up);
+        panelPrefab.transform.SetPositionAndRotation(spawnPos, spawnRot);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public TMP_Text displayText;
+    private string currentText = "";
+    
+    public void addDigit(string digit)
     {
-        
+        currentText += digit;
+        UpdateDisplay();
     }
+
+    public void removeDigitlast()
+    {
+        if (currentText.Length > 0)
+        {
+            currentText = currentText.Substring(0, currentText.Length - 1);
+            UpdateDisplay();
+        }
+    }
+
+
+
 }
